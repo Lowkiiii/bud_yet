d18bc07cb6875eeb3803790f024a45447a1e09d5
rdeniele <rondenielep13@gmail.com>
2024-06-27 15:13:43 +0800

    added models

---
+++ app/Models/tbl_income.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_income extends Model
+{
+    use HasFactory;
+    // protected $fillable= ['add_on_name','add_on_price','created_at', 'updated_at',];
+}
