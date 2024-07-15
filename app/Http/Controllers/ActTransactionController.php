702f6c87063c07586b45391b0ef144876c1a2771
rdeniele <rondenielep13@gmail.com>
2024-07-15 13:06:17 +0800

    fixed dashboard values

--- app/Http/Controllers/ActTransactionController.php
+++ app/Http/Controllers/ActTransactionController.php
@@ -78,28 +78,11 @@ class ActTransactionController extends Controller
             ->select('transaction.*', 'accounts.*')
             ->where('accounts.user_id', Auth::id())
             ->join('tbl_account AS accounts', 'transaction.account_id', '=', 'accounts.id')
-            ->orderBy('date', 'desc')
+            ->orderBy('transaction.created_at', 'desc') // Specify the table alias here
             ->get();
-
-        // var_dump($transactions);
-
         $accounts = Account::where('user_id', Auth::id())->get();
-
         return view('transaction.transaction', compact('transactions', 'accounts'));
     }
-    // public function dashboard()
-    // {
-    //     $transactions = DB::table('tbl_account_transaction AS transaction')
-    //         ->select('transaction.*', 'accounts.*')
-    //         ->where('accounts.user_id', Auth::id())
-    //         ->join('tbl_account AS accounts', 'transaction.account_id', '=', 'accounts.id')
-    //         ->orderBy('date', 'desc')
-    //         ->get();
-
-    //     $account = Account::where('user_id', auth()->id())->get();
-
-    //     return view('dashboard.dashboard', compact('transactions', 'account'));
-    // }
 
     public function dashboard()
     {
@@ -134,7 +117,7 @@ class ActTransactionController extends Controller
             ->select('transaction.*', 'accounts.*')
             ->where('accounts.user_id', Auth::id())
             ->join('tbl_account AS accounts', 'transaction.account_id', '=', 'accounts.id')
-            ->orderBy('date', 'desc')
+            ->orderBy('transaction.created_at', 'desc')
             ->get();
 
         // total deposit
